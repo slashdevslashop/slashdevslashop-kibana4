2f59a4681f7bf851e0d06226c5e8b9920a37a946
xiii <efstathios.xagoraris@itv.com>
2015-03-10 21:13:08 +0000

    Added kibana4 download and test presence

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,9 +2,25 @@
 #
 # This class is called from kibana4 for install.
 #
-class kibana4::install {
+class kibana4::install(
+	$version,
+	$arch
+	) {
 
-  package { $::kibana4::package_name:
-    ensure => present,
+  exec {"check_presence":
+    command => '/bin/true',
+    onlyif => '/usr/bin/test -e /opt/kibana4',
   }
+
+  archive { "kibana4":
+   ensure => present,
+   url => "https://download.elasticsearch.org/kibana/kibana/kibana-${version}-linux-${arch}.tar.gz",
+   target => '/opt/kibana4',
+   follow_redirects => true,
+   extension => 'tgz',
+   checksum => false,
+   require => Exec["check_presence"],
+
+  }
+
 }
