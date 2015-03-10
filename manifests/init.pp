57ed9e7deee11b4379e425c60c6f05c86cbb521d
xiii <efstathios.xagoraris@itv.com>
2015-03-10 21:12:24 +0000

    Pass version & arch to the module

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,13 +8,17 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class kibana4 (
-  $package_name = $::kibana4::params::package_name,
+  $version = $::kibana4::params::version,
+  $arch = $::kibana4::params::arch,
   $service_name = $::kibana4::params::service_name,
 ) inherits ::kibana4::params {
 
   # validate parameters here
 
-  class { '::kibana4::install': } ->
+  class { '::kibana4::install': 
+  	version => $version,
+  	arch => $arch
+  } ->
   class { '::kibana4::config': } ~>
   class { '::kibana4::service': } ->
   Class['::kibana4']
